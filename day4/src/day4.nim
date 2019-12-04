a0061cde83addc5fcf41e1ddfc93734d490afb3d
Daniel Meszaros <easimer@gmail.com>
2019-12-04 09:02:11 +0100

    Day 4: optimize part 2

--- day4/src/day4.nim
+++ day4/src/day4.nim
@@ -50,15 +50,30 @@ func passwordOk2(pass: int): bool =
     hasDoubleGroup = true
   result = result and hasDoubleGroup
 
-proc worker(x: (proc(p: int): bool), first: int, last: int): int =
-  for candidate in first .. last:
-    if x(candidate): result = result + 1
+type
+  WorkKind = enum
+    Range, Seq
+  Day4Work = object
+    f: (proc(p: int): bool)
+    case kind: WorkKind
+      of Range:
+        _: int
+      of Seq:
+        S: seq[int]
+
+proc worker(work: Day4Work, first: int, last: int): seq[int] =
+  case work.kind:
+    of Range:
+      for candidate in first .. last:
+        if passwordOk(candidate): result.add(candidate)
+    of Seq:
+      for i in first .. last:
+        if passwordOk2(work.S[i]): result.add(work.S[i])
 
 proc readRange(): (int, int) =
   let inputPath = if paramCount() > 0: paramStr(1) else: "input.txt"
   var f: File = open(inputPath)
   defer: f.close()
-
   result = (parseInt(f.readLine()), parseInt(f.readLine()))
 
 when isMainModule:
@@ -66,12 +81,18 @@ when isMainModule:
   let passRange = readRange()
   let inputEnd = getTime()
 
+  var W1: Day4Work
+  W1.kind = WorkKind.Range
   let part1Start = getTime()
-  let output1 = distributeWork(worker, passwordOk, passRange[0], passRange[1]).foldl(a+b)
+  let validPasswords = distributeWork(worker, W1, passRange[0], passRange[1]).foldl(a & b)
+  let output1 = len(validPasswords)
   let part1End = getTime()
 
+  var W2: Day4Work
+  W2.kind = WorkKind.Seq
+  W2.S = validPasswords
   let part2Start = getTime()
-  let output2 = distributeWork(worker, passwordOk2, passRange[0], passRange[1]).foldl(a+b)
+  let output2 = len(distributeWork(worker, W2, 0, validPasswords.high()).foldl(a & b))
   let part2End = getTime()
   
   var R: AOCResults
