223f2f1009979a7c4ef972bd716be51a97f0c2ce
Daniel Meszaros <easimer@gmail.com>
2019-12-05 17:48:46 +0100

    intcode: improve code

--- aocutils/aocutils.nimble
+++ aocutils/aocutils.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.2.1"
+version       = "0.2.2"
 author        = "Daniel Meszaros"
 description   = "AoC 2019 utilities"
 license       = "MIT"
--- aocutils/src/aocutils/intcode.nim
+++ aocutils/src/aocutils/intcode.nim
@@ -7,22 +7,20 @@ type
     nextPC*: int
     halt*: bool
     output*: Option[int]
-
-const ADD = 1
-const MUL = 2
-const IN  = 3
-const OUT = 4
-const JNZ = 5
-const JZ  = 6
-const LT  = 7
-const EQ  = 8
-const HLT = 99
+  
+  Opcode = enum
+    ADD = 1, MUL = 2, IN = 3, OUT = 4, JNZ = 5, JZ = 6,
+    LT = 7, EQ = 8,
+    HLT = 99
+  
+  DecodedInstruction = tuple[opcode: Opcode, mode0, mode1, mode2: int]
 
 template `$>`(address: untyped): untyped = memory[address]
 template `$>>`(address: untyped): untyped = $> $> address
 
-func decodeInstruction(instr: int): tuple[opcode, mode0, mode1, mode2: int] =
-  result.opcode = instr mod 100
+func decodeInstruction(instr: int): DecodedInstruction =
+  ## Decodes the instruction, returning the opcode and the argument modes.
+  result.opcode = Opcode(instr mod 100)
   result.mode0 = instr div 100
   result.mode1 = result.mode0 div 10
   result.mode0 = result.mode0 mod 10
@@ -30,66 +28,74 @@ func decodeInstruction(instr: int): tuple[opcode, mode0, mode1, mode2: int] =
   result.mode1 = result.mode1 mod 10
   result.mode2 = result.mode2 mod 10
 
-assert decodeInstruction(    1) == (1, 0, 0, 0)
-assert decodeInstruction(  101) == (1, 1, 0, 0)
-assert decodeInstruction( 1001) == (1, 0, 1, 0)
-assert decodeInstruction( 1101) == (1, 1, 1, 0)
-assert decodeInstruction(10001) == (1, 0, 0, 1)
-assert decodeInstruction(10101) == (1, 1, 0, 1)
-assert decodeInstruction(11001) == (1, 0, 1, 1)
-assert decodeInstruction(11101) == (1, 1, 1, 1)
+assert decodeInstruction(    1) == (ADD, 0, 0, 0)
+assert decodeInstruction(  101) == (ADD, 1, 0, 0)
+assert decodeInstruction( 1001) == (ADD, 0, 1, 0)
+assert decodeInstruction( 1101) == (ADD, 1, 1, 0)
+assert decodeInstruction(10001) == (ADD, 0, 0, 1)
+assert decodeInstruction(10101) == (ADD, 1, 0, 1)
+assert decodeInstruction(11001) == (ADD, 0, 1, 1)
+assert decodeInstruction(11101) == (ADD, 1, 1, 1)
 
-template readValueMode(argIdx: int, instr: (int, int, int, int), memory: seq[int]): int =
+template `@`(argIdx: int): int =
+  ## Gets an argument value, taking into account the argument mode.
+  ## argIdx==1 returns the first argument, argIdx==2 the second and so on.
   case instr[argIdx]:
-    of 0: $> $> (pc + argIdx)
+    of 0: $>> (pc + argIdx)
     of 1: $> (pc + argIdx)
     else: raise newException(ValueError, "Invalid mode " & $instr[argIdx])
 
+template incPC(op: Opcode): int =
+  ## Returns the PC offset for opcodes with fixed offset
+  case op:
+    of ADD: 4
+    of MUL: 4
+    of LT: 4
+    of EQ: 4
+    of IN: 2
+    of OUT: 2
+    else: 0
+
 template nextPC(value: untyped): untyped =
+  ## Sets the program counter
   result.nextPC = value
 
 func executeInstruction*(inp, pc: int, memory: var seq[int]): InterpreterResult =
-  result.halt = false
+  result.nextPC = pc
   let instr = decodeInstruction($>(pc + 0))
   case instr.opcode:
     of ADD:
-      $>>(pc + 3) = readValueMode(1, instr, memory) + readValueMode(2, instr, memory)
-      nextPC(pc + 4)
+      $>>(pc + 3) = @1 + @2
     of MUL:
-      $>>(pc + 3) = readValueMode(1, instr, memory) * readValueMode(2, instr, memory)
-      nextPC(pc + 4)
+      $>>(pc + 3) = @1 * @2
     of IN:
       $>>(pc + 1) = inp
-      nextPC(pc + 2)
     of OUT:
       result.output = some($>>(pc + 1))
-      nextPC(pc + 2)
     of HLT:
       result.halt = true
     of JNZ:
-      if readValueMode(1, instr, memory) != 0:
-        nextPC(readValueMode(2, instr, memory))
+      if @1 != 0:
+        nextPC(@2)
       else:
         nextPC(pc + 3)
     of JZ:
-      if readValueMode(1, instr, memory) == 0:
-        nextPC(readValueMode(2, instr, memory))
+      if @1 == 0:
+        nextPC(@2)
       else:
         nextPC(pc + 3)
     of LT:
-      if readValueMode(1, instr, memory) < readValueMode(2, instr, memory):
+      if @1 < @2:
         $>>(pc + 3) = 1
       else:
         $>>(pc + 3) = 0
-      nextPC(pc + 4)
     of EQ:
-      if readValueMode(1, instr, memory) == readValueMode(2, instr, memory):
+      if @1 == @2:
         $>>(pc + 3) = 1
       else:
         $>>(pc + 3) = 0
-      nextPC(pc + 4)
-    else:
-      raise newException(ValueError, "Unknown opcode in " & $instr)
+  result.nextPC += incPC(instr.opcode)
+  if not result.halt: assert(result.nextPC != pc, "Program counter has not been modified! PC=" & $pc & " OP=" & $instr.opcode & " Invalid opcode or spinning?")
 
 proc readProgramFromPath*(path: string): seq[int] =
   var f = open(path)
