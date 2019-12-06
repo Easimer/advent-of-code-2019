04358dcf1e0a11198d51c99cbfe47706939e2c60
Daniel Meszaros <easimer@gmail.com>
2019-12-06 12:19:07 +0100

    Day 6: code cleanup

--- day6/src/day6.nim
+++ day6/src/day6.nim
@@ -1,7 +1,9 @@
+import aocutils
 import os
 import sets
 import strutils
 import tables
+import times
 
 type
   Orbit = tuple[lhs, rhs: int]
@@ -18,7 +20,10 @@ func newOrbit(lhs: string, rhs: string): Orbit =
   result.lhs = bodyNameToInt(lhs)
   result.rhs = bodyNameToInt(rhs)
 
-proc loadOrbits(f: var File): seq[Orbit] =
+proc loadOrbits(): seq[Orbit] =
+  let inputPath = if paramCount() > 0: paramStr(1) else: "input.txt"
+  var f = open(inputPath)
+  defer: f.close()
   for line in f.lines():
     let rawOrbit = line.split(')')
     result.add(newOrbit(rawOrbit[1], rawOrbit[0]))
@@ -29,6 +34,13 @@ func countIndirectOrbits(body: int, M: OrbitMap): int =
   while orbited in M:
     orbited = M[orbited]
     result += 1
+
+func pathFromToAnyIn(f: int, C: HashSet[int], M: OrbitMap): seq[int] =
+  var nextBody = M[f]
+  while nextBody in M and not (nextBody in C):
+    result.add(nextBody)
+    nextBody = M[nextBody]
+  result.add(nextBody)
 
 func part2(M: OrbitMap): int =
   var setYou: HashSet[int] # All bodies from YOU to COM
@@ -43,41 +55,33 @@ func part2(M: OrbitMap): int =
     setSan.incl(orbited)
     orbited = M[orbited]
   let setCom = intersection(setYou, setSan)
-  debugEcho(setCom)
 
-  var pathYouToMain: seq[int] # Path from to you to the first point in the main orbit transfer branch
-  var pathSanToMain: seq[int] # Path from to Santa to the first point in the main orbit transfer branch
-  
-  var nextBody = M[BODY_YOU]
-  while nextBody in M and not (nextBody in setCom):
-    pathYouToMain.add(nextBody)
-    nextBody = M[nextBody]
-  pathYouToMain.add(nextBody)
+  let pathYouToMain = pathFromToAnyIn(BODY_YOU, setCom, M)
+  let pathSanToMain = pathFromToAnyIn(BODY_SAN, setCom, M)
 
-  nextBody = M[BODY_SAN]
-  while nextBody in M and not (nextBody in setCom):
-    pathSanToMain.add(nextBody)
-    nextBody = M[nextBody]
-  pathSanToMain.add(nextBody)
-
-  #debugEcho((pathYouToMain, pathSanToMain))
   # Sub one because both lists contain the first common point, then
   # sub another one because we need the number of the edges
   len(pathYouToMain) + len(pathSanToMain) - 2
 
 when isMainModule:
-  let inputPath = if paramCount() > 0: paramStr(1) else: "input.txt"
-  var f = open(inputPath)
-  let orbits = loadOrbits(f)
+  let parseStart = getTime()
+  let orbits = loadOrbits()
   let orbitMap = OrbitMap(toTable(orbits))
+  let parseEnd = getTime()
 
+  let part1Start = getTime()
   let directOrbits = len(orbits)
   var indirectOrbits = 0
   for k, v in orbitMap:
     indirectOrbits += countIndirectOrbits(k, orbitMap)
-
   let output1 = $(directOrbits + indirectOrbits)
-  echo(output1)
-  echo(part2(orbitMap))
+  let part1End = getTime()
 
-  f.close()
+  let part2Start = getTime()
+  let output2 = $part2(orbitMap)
+  let part2End = getTime()
+
+  var R: AOCResults
+  R.init(output1, output2, float inMicroseconds(parseEnd - parseStart), float inMicroseconds(part1End - part1Start), float inMicroseconds(part2End - part2Start))
+  printResults(R)
+
