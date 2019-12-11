36abc420cbd74e8c204c38ff998e6a2df6ac93d5
Daniel Meszaros <easimer@gmail.com>
2019-12-11 09:23:08 +0100

    Day 11: cleanup

--- day11/src/day11.nim
+++ day11/src/day11.nim
@@ -5,10 +5,8 @@ import options
 import json
 
 type
-  Tile = enum
-    Black, White
-  RobotMode = enum
-    Paint, Turn
+  Tile   = enum Black = 0, White = 1
+  Action = enum Paint, Turn
 
 func getTile(pos: (int, int), map: Table[(int, int), Tile]): Tile =
   if pos in map:
@@ -16,38 +14,29 @@ func getTile(pos: (int, int), map: Table[(int, int), Tile]): Tile =
   else:
     Black
 
-proc run(origProgram: Memory, startOn = Black): (int, string) =
+proc run(origProgram: Memory, startOn: Tile): (int, string) =
   var state: InterpreterResult
   var program = origProgram
 
   var map: Table[(int, int), Tile]
   var pos: (int, int) = (0, 0)
   var dir = 1 # Up
-  var mode = Paint # Paint mode
+  var mode = Paint
   map[pos] = startOn
 
   while not state.halt:
-    let input = if getTile(pos, map) == White: 1 else: 0
+    let input = int(getTile(pos, map))
     state = executeInstruction(input, state.nextPC, program, state.base)
     if state.output.isSome():
       case mode:
         of Paint:
-          case state.output.get():
-            of 0:
-              map[pos] = Black
-            of 1:
-              map[pos] = White
-            else: raise newException(ValueError, "FUG")
+          map[pos] = Tile(state.output.get())
           mode = Turn
         of Turn:
-          case state.output.get():
-            of 0:
-              dir = (dir + 1)
-              if dir > 3: dir -= 4
-            of 1:
-              dir = (dir - 1) mod 4
-              if dir < 0: dir += 4
-            else: raise newException(ValueError, "FUG")
+          dir += -(state.output.get() * 2) + 1
+          if dir > 3: dir -= 4
+          if dir < 0: dir += 4
+
           case dir:
             of 0: pos[0] += 1
             of 1: pos[1] += 1
@@ -59,7 +48,7 @@ proc run(origProgram: Memory, startOn = Black): (int, string) =
   for k, v in map:
     result[0] += 1
 
-  for y in -5 .. 5:
+  for y in countdown(5, -5):
     for x in 0 .. 40:
       if (x, y) in map:
           result[1] &= (if map[(x, y)] == White: '#' else: '.')
