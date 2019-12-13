cbdcc7fa68dc657b32209b6ccfd6b8f41bee9783
Daniel Meszaros <easimer@gmail.com>
2019-12-13 10:48:05 +0100

    Day 13: solve

---
+++ day13/day13.nimble
@@ -0,0 +1,16 @@
+# Package
+
+version       = "0.1.0"
+author        = "Daniel Meszaros"
+description   = "Day 13"
+license       = "GPL-3.0"
+srcDir        = "src"
+bin           = @["day13.exe"]
+
+
+
+# Dependencies
+
+requires "nim >= 1.0.0"
+requires "aocutils >= 0.4.0"
+requires "gifwriter >= 0.1.0"
---
+++ day13/src/day13.nim
@@ -0,0 +1,138 @@
+import os
+import options
+import tables
+import gifwriter
+import aocutils/intcode
+
+type
+  VMState = enum OutX, OutY, OutT
+  Tile = enum Empty = 0, Wall = 1, Block = 2, Paddle = 3, Ball = 4
+  Point = tuple[x, y: int]
+
+const BOARDSIZ = 64
+
+func `+`(lhs: VMState, rhs: int): VMState =
+  assert rhs == 1
+  case lhs:
+    of OutX: OutY
+    of OutY: OutT
+    of OutT: OutX
+
+proc renderBoard(g: Gif, board: seq[Tile], width: int) =
+  var pixels = newSeq[Color](width * width)
+
+  for y in 0 .. width - 1:
+    for x in 0 .. width - 1:
+      pixels[y * width + x] =
+        case board[y * width + x]:
+          of Empty: Color(r: 0, g: 0, b: 0)
+          of Wall: Color(r: 255, g: 255, b: 255)
+          of Block: Color(r: 127, g: 127, b: 127)
+          of Paddle: Color(r: 0, g: 255, b: 0)
+          of Ball: Color(r: 255, g: 0, b: 0)
+  g.write(pixels)
+
+func part1(program: Memory): int =
+  var state: InterpreterResult
+  var mem = program
+  var ioState = OutX
+  var buf: Point
+  var pixels = newSeq[Tile](BOARDSIZ * BOARDSIZ)
+  
+  while not state.halt:
+    let input = 0
+    state = executeInstruction(input, state.nextPC, mem, state.base)
+
+    if isSome(state.output):
+      case ioState:
+        of OutX:
+          buf.x = state.output.get()
+        of OutY:
+          buf.y = state.output.get()
+        of OutT:
+          let t = state.output.get()
+          pixels[buf.y * BOARDSIZ + buf.x] = Tile(t)
+      ioState = ioState + 1
+
+
+  for pixel in pixels:
+    if pixel == Block: result += 1
+
+func part2(program: Memory): int =
+  var state: InterpreterResult
+  var mem = program
+  var ioState = OutX
+  var buf, paddle, ball, ballDelta: Point
+  var pixels = newSeq[Tile](BOARDSIZ * BOARDSIZ)
+  var input = 0
+  var g = newGif("output.gif", BOARDSIZ, BOARDSIZ, colors = 8, fps = 30)
+  defer: close(g)
+
+  mem[0] = 2
+  renderBoard(g, pixels, BOARDSIZ)
+
+  while not state.halt:
+    state = executeInstruction(input, state.nextPC, mem, state.base)
+
+    if isSome(state.output):
+      case ioState:
+        of OutX:
+          buf.x = state.output.get()
+        of OutY:
+          buf.y = state.output.get()
+        of OutT:
+          let o = state.output.get()
+          if buf.x > -1:
+            let t = Tile(o)
+            pixels[buf.y * BOARDSIZ + buf.x] = t
+            if t == Paddle:
+              paddle.x = buf.x
+              paddle.y = buf.y
+              renderBoard(g, pixels, BOARDSIZ)
+            elif t == Ball:
+              ballDelta.x = buf.x - ball.x
+              ballDelta.y = buf.y - ball.y
+              ball.x = buf.x
+              ball.y = buf.y
+              renderBoard(g, pixels, BOARDSIZ)
+          else:
+            debugEcho(o)
+            result = o
+      ioState = ioState + 1
+
+    #[
+      b.x + t * d.x = f.x
+      b.y + t * d.y = paddle.y
+      t = (paddle.y - b.y) / d.y
+    ]#
+
+    if ballDelta.y > 0:
+      input =
+        if paddle.x < ball.x: 1
+        elif ball.x < paddle.x: -1
+        else: 0
+    elif ballDelta.y < 0:
+      var coll: Point = ball
+      while pixels[coll.y * BOARDSIZ + coll.x] == Empty:
+        coll.y += ballDelta.y
+        coll.x += ballDelta.x
+      
+      var futureDelta: Point
+      futureDelta.x = ballDelta.x
+      futureDelta.y = -ballDelta.y
+
+      let t = (paddle.y - coll.y) / futureDelta.y
+      let fx = float(coll.x) + t * float(futureDelta.x)
+      let px = float(paddle.x)
+      input =
+        if px < fx: 1
+        elif fx < px: -1
+        else: 0
+    else:
+      input = 0
+
+
+when isMainModule:
+  let program = readProgramFromPath(if paramCount() > 0: paramStr(1) else: "input.txt")
+  echo part1(program)
+  echo part2(program)
